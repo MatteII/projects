4ec96753d99db860cf0cbf7d4616422eacb28593
Matte <matteo.desandre01@gmail.com>
2023-11-25 17:09:44 +0100

    index

---
+++ PHP/PhpStarter/index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="it">
+<head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Php Starter</title>
+</head>
+<body style="display: grid; place-items: center; height: 100vh; font-family: sans-serif;" >
+        
+        <h1><?php echo "Hello World!"; ?></h1>
+
+</body>
+</html>
